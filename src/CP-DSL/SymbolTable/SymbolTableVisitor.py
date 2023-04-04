10b11910839a716c03bbb8aca0912267d11e3a22
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-04-04 16:54:27 +0200

    - update Imports - comment  group decl - upate name attribute of featureDeclaration - repplace scope change with visit walker
    - TODO rm scope change as var decl should not change the scope

--- src/CP-DSL/SymbolTable/SymbolTableVisitor.py
+++ src/CP-DSL/SymbolTable/SymbolTableVisitor.py
@@ -2,12 +2,12 @@ from typing import TypeVar, Generic, Dict, Optional, Callable, Any
 
 from antlr4.tree.Tree import ParseTree
 
-from SymbolTable.SymbolTable import ScopedSymbol, SymbolTable, P, T, BlockSymbol, RoutineSymbol, SymbolTableOptions, ParameterSymbol
-from Configuration.DeclarationParser import DeclarationParser
-from Configuration.DeclarationVisitor import DeclarationVisitor
+from SymbolTable.SymbolTable import SymbolTable, P, T, BlockSymbol, RoutineSymbol, SymbolTableOptions, VariableSymbol
+from Declaration.DeclarationParser import DeclarationParser
+from Declaration.DeclarationVisitor import DeclarationVisitor
 
 
-class SymbolTableVisitor( DeclarationVisitor, Generic[T] ):
+class CP_DSLSymbolTableVisitor( DeclarationVisitor, Generic[T] ):
     _symbolTable: SymbolTable
 
     def __init__(self, name: str = '', ):
@@ -25,26 +25,40 @@ class SymbolTableVisitor( DeclarationVisitor, Generic[T] ):
     def defaultResult(self) -> SymbolTable:
         return self._symbolTable
 
-    def visitParamAssignStat(self, ctx: DeclarationParser.ParamAssignStatContext):
-        
+    # Visit a parse tree produced by DeclarationParser#paramAssignStat.
+    def visitParamAssignStat(self, ctx:DeclarationParser.ParamAssignStatContext):
+
         # define the given Parameter
-        t = ParameterSymbol # set and get the type of param here
+        # TODO merge UnitSymbol from main and change t to UnitSymbol
+        t = VariableSymbol # set and get the type of param here
         varName = ctx.ID().getText() # set and get the variable name here
-        
+
+
+        # TODO add defaultValue and unit see
+        #  The_Definitive_ANTLR_4_Reference.pdf 56/322 bzw 40
+        #       https://git.se.informatik.uni-kiel.de/oceandsl/organization/-/wikis/uploads/30c8a43db807b74d17147b69360e1566/The_Definitive_ANTLR_4_Reference.pdf
+        #  or example : media.pragprog.com/titles/tpantlr2/code/tour/EvalVisitor.java
+        # TODO backlog add description as comment to SymbolTable?
         scope = self._symbolTable.addNewSymbolOfType(t, self._scope, varName)
-        scope.context = ctx
-        self._scope = scope
-        self._scope = scope.parent()
-        return scope
-        
-    def visitParamGroupAssignStat(self, ctx: DeclarationParser.ParamGroupAssignStatContext):
-        return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
-        
+        # scope.context = ctx
+        # self._scope = scope
+        # self._scope = scope.parent()
+        # return scope
+        return self.visitChildren( ctx )
+
+    # TODO backlog visitParamGroupAssignStat as BlockSymbol is not implemented and do we need it?
+    # def visitParamGroupAssignStat(self, ctx: DeclarationParser.ParamGroupAssignStatContext):
+    #     return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
+
+    # CHANGES sgu:
+    #   featureDeclaration ID is saved as attribute 'name'
+
     def visitFeatureAssignStat(self, ctx: DeclarationParser.FeatureAssignStatContext):
-        return self.withScope(ctx, RoutineSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
-        
-    def visitFeatureGroupAssignStat(self, ctx: DeclarationParser.FeatureGroupAssignStatContext):
-        return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
+        return self.withScope(ctx, RoutineSymbol, lambda: self.visitChildren(ctx), ctx.name.text)
+
+    # TODO backlog visitFeatureGroupAssignStat as BlockSymbol is not implemented and do we need it?
+    # def visitFeatureGroupAssignStat(self, ctx: DeclarationParser.FeatureGroupAssignStatContext):
+    #     return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
 
     # def visitAssignStat(self, ctx: TestGrammarParser.AssignStatContext):
     #     self._symbolTable.addNewSymbolOfType( VariableSymbol, self._scope, ctx.ID().getText() )
@@ -52,7 +66,7 @@ class SymbolTableVisitor( DeclarationVisitor, Generic[T] ):
 
     # def visitFuncExpr(self, ctx: TestGrammarParser.FuncExprContext):
     #     return self.withScope( ctx, RoutineSymbol, lambda: self.visitChildren( ctx ), ctx.ID().getText() )
-    
+
     def withScope(self, tree: ParseTree, t: type, action: Callable, *my_args: P.args or None,
                   **my_kwargs: P.kwargs or None) -> T:
         scope = self._symbolTable.addNewSymbolOfType( t, self._scope, *my_args, **my_kwargs )

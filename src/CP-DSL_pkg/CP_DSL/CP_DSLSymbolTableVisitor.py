d16cb45fd1f05b2e1af6cdfaf673549c10416f2a
stu222808 <a.mackensen@web.de>
2023-04-13 12:41:38 +0200

    add Featuredescription feature and group assignment scopes

--- src/CP-DSL_pkg/CP_DSL/CP_DSLSymbolTableVisitor.py
+++ src/CP-DSL_pkg/CP_DSL/CP_DSLSymbolTableVisitor.py
@@ -30,18 +30,10 @@ class CP_DSLSymbolTableVisitor( DeclarationVisitor, Generic[T] ):
     def visitParamAssignStat(self, ctx:DeclarationParser.ParamAssignStatContext):
         
         # define the given Parameter
-        # TODO merge UnitSymbol from main and change t to UnitSymbol
-        t = VariableSymbol # set and get the type of param here
         varName = ctx.name.text # set and get the variable name here
         unit = self.visit(ctx.unit)
         description = ctx.description.text
         scope = self._symbolTable.addNewSymbolOfType(VariableSymbol, varName, ctx, unit)
-
-
-        # TODO add defaultValue and unit see
-        #  The_Definitive_ANTLR_4_Reference.pdf 56/322 bzw 40
-        #       https://git.se.informatik.uni-kiel.de/oceandsl/organization/-/wikis/uploads/30c8a43db807b74d17147b69360e1566/The_Definitive_ANTLR_4_Reference.pdf
-        #  or example : media.pragprog.com/titles/tpantlr2/code/tour/EvalVisitor.java
         # TODO backlog add description as comment to SymbolTable?
         return scope
     
@@ -64,24 +56,22 @@ class CP_DSLSymbolTableVisitor( DeclarationVisitor, Generic[T] ):
     
     # customUnit                  :   name=STRING #customunit;
     def visitCustomunit(self, ctx:DeclarationParser.customUnit):
-        # TODO: Set Prefix and type in customUnit also
+        # TODO: Set Prefix and type in customUnit either
         return FundamentalUnit(name = ctx.name.text)
     
         
-
-    # TODO backlog visitParamGroupAssignStat as BlockSymbol is not implemented and do we need it?
-    # def visitParamGroupAssignStat(self, ctx: DeclarationParser.ParamGroupAssignStatContext):
-    #     return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
+    def visitParamGroupAssignStat(self, ctx: DeclarationParser.ParamGroupAssignStatContext):
+        return self.withScope(ctx, RoutineSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
 
     # CHANGES sgu:
     #   featureDeclaration ID is saved as attribute 'name'
 
     def visitFeatureAssignStat(self, ctx: DeclarationParser.FeatureAssignStatContext):
+        description = ctx.description.text
         return self.withScope(ctx, RoutineSymbol, lambda: self.visitChildren(ctx), ctx.name.text)
 
-    # TODO backlog visitFeatureGroupAssignStat as BlockSymbol is not implemented and do we need it?
-    # def visitFeatureGroupAssignStat(self, ctx: DeclarationParser.FeatureGroupAssignStatContext):
-    #     return self.withScope(ctx, BlockSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
+    def visitFeatureGroupAssignStat(self, ctx: DeclarationParser.FeatureGroupAssignStatContext):
+        return self.withScope(ctx, RoutineSymbol, lambda: self.visitChildren(ctx), ctx.ID().getText())
 
     # def visitAssignStat(self, ctx: TestGrammarParser.AssignStatContext):
     #     self._symbolTable.addNewSymbolOfType( VariableSymbol, self._scope, ctx.ID().getText() )

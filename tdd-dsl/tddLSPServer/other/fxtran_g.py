c889d0d57b073d61237bd3101e108fe4a0074f38
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-05-24 10:21:37 +0200

    add match case elif variant

--- tdd-dsl/tddLSPServer/other/fxtran_g.py
+++ tdd-dsl/tddLSPServer/other/fxtran_g.py
@@ -21,40 +21,62 @@ ns = {'fx': 'http://fxtran.net/#syntax'}
 variables = [ ]
 scope_stack = [ ]  # Stack to track the current scope
 
+# # on-off match guard syntax https://peps.python.org/pep-0622/#one-off-syntax-variant
+# # Iterate over all elements in the XML tree
+# for element in root.iter( ):
+#     match element:
+#         case element if element.tag.endswith( 'end-subroutine-stmt' ) | element.tag.endswith( 'end-program-stmt' ) | element.tag.endswith( 'end-function-stmt' ):
+#             scope_stack.pop( )
+#         case element if element.tag.endswith( 'subroutine-stmt' ) | element.tag.endswith( 'program-stmt' ) | element.tag.endswith( 'function-stmt' ):
+#             scope_name = element.find( './/fx:n', ns ).text
+#             scope_stack.append( scope_name )
+#         case element if element.tag.endswith( 'T-decl-stmt' ):
+#             # Check if variable is not an output
+#             intent_spec = element.find( './/fx:intent-spec', ns )
+#             if intent_spec is None or intent_spec.text != 'out':
+#                 # Get the current scope from the scope stack
+#                 current_scope = '.'.join( scope_stack )
+#
+#                 # Get the type of the variable if it exists
+#                 t_spec_element = element.find( './/fx:T-N', ns )
+#                 variable_type = t_spec_element.text if t_spec_element is not None else ''
+#
+#                 for en_decl in element.findall( './/fx:EN-decl', ns ):
+#                     # Get the name of the variable from the named element
+#                     variable_name = en_decl.find( './/fx:n', ns ).text
+#
+#                     # Save the variable names with their respective types and scopes
+#                     variables.append( (variable_name, variable_type, current_scope) )
+
 # Iterate over all elements in the XML tree
 for element in root.iter( ):
 
     # Reduce the scope stack when leaving scopes
     if element.tag.endswith( 'end-subroutine-stmt' ) or element.tag.endswith( 'end-program-stmt' ) or element.tag.endswith( 'end-function-stmt' ):
         scope_stack.pop( )
-    else:
+    elif element.tag.endswith( 'subroutine-stmt' ) or element.tag.endswith( 'program-stmt' ) or element.tag.endswith( 'function-stmt' ):
+        # Find named element
+        scope_name = element.find( './/fx:n', ns ).text
+        scope_stack.append( scope_name )
+    elif element.tag.endswith( 'T-decl-stmt' ):
+        # Check if variable is not an output
+        intent_spec = element.find( './/fx:intent-spec', ns )
+        if intent_spec is None or intent_spec.text != 'out':
+            # Get the current scope from the scope stack
+            current_scope = '.'.join( scope_stack )
 
-        # Extend the scope stack when encountering new scopes
-        if element.tag.endswith( 'subroutine-stmt' ) or element.tag.endswith( 'program-stmt' ) or element.tag.endswith( 'function-stmt' ):
-            # Find named element
-            scope_name = element.find( './/fx:n', ns ).text
-            scope_stack.append( scope_name )
-        else:
+            # Get the type of the variable if it exists
+            t_spec_element = element.find( './/fx:T-N', ns )
+            variable_type = t_spec_element.text if t_spec_element is not None else ''
 
-            if element.tag.endswith( 'T-decl-stmt' ):
-                # Check if variable is not an output
-                intent_spec = element.find( './/fx:intent-spec', ns )
-                if intent_spec is None or intent_spec.text != 'out':
-                    # Get the current scope from the scope stack
-                    current_scope = '.'.join( scope_stack )
+            for en_decl in element.findall( './/fx:EN-decl', ns ):
+                # Get the name of the variable from the named element
+                variable_name = en_decl.find( './/fx:n', ns ).text
 
-                    # Get the type of the variable if it exists
-                    t_spec_element = element.find( './/fx:T-N', ns )
-                    variable_type = t_spec_element.text if t_spec_element is not None else ''
-
-                    for en_decl in element.findall( './/fx:EN-decl', ns ):
-                        # Get the name of the variable from the named element
-                        variable_name = en_decl.find( './/fx:n', ns ).text
-
-                        # Save the variable names with their respective types and scopes
-                        variables.append( (variable_name, variable_type, current_scope) )
+                # Save the variable names with their respective types and scopes
+                variables.append( (variable_name, variable_type, current_scope) )
 
 # TODO Debug
 # Print the list of variable names with their respective types and scopes
 for variable, variable_type, scope in variables:
-    print( f"Variable: {variable}\t Type: {variable_type}\t Scope: {scope}" )
+    print( f"elIf Variable: {variable}\t Type: {variable_type}\t Scope: {scope}" )

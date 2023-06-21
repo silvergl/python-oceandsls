3d18f2fd54d237e43f7e4ad240e63239fc82f856
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-06-21 13:29:43 +0200

    update antlr4 version

--- example-dsl/exampleLSPServer/gen/python/exampleDsl/exampleDslVisitor.py
+++ example-dsl/exampleLSPServer/gen/python/exampleDsl/exampleDslVisitor.py
@@ -1,6 +1,6 @@
-# Generated from exampleDsl.g4 by ANTLR 4.12.0
+# Generated from exampleDsl.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .exampleDslParser import exampleDslParser
 else:
     from exampleDslParser import exampleDslParser

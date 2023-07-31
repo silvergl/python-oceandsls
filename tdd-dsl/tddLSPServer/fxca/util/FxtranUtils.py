d3b01dfafe89ed5ae81e60bb2e03d329fc673f49
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-07-31 14:30:00 +0200

    TDD-DSL: Update Public F90 parsing

--- tdd-dsl/tddLSPServer/fxca/util/FxtranUtils.py
+++ tdd-dsl/tddLSPServer/fxca/util/FxtranUtils.py
@@ -14,6 +14,7 @@ from typing import Dict, List, Tuple
 
 from symbolTable.SymbolTable import ModuleSymbol
 
+
 @dataclass
 class PublicObj:
     """
@@ -25,9 +26,9 @@ class PublicObj:
     # By default, entities are public unless general private statement
     defaultPrivate: bool = False
     # Entities specifically mentioned as public optionally with attributes
-    pubElements: Dict[ str, List[str] ] = field( default_factory = dict )
+    pubElements: Dict[ str, List[ str ] ] = field( default_factory = dict )
     # Entities specifically mentioned as private optionally with attributes
-    prElements: Dict[ str, List[str] ] = field( default_factory = dict )
+    prElements: Dict[ str, List[ str ] ] = field( default_factory = dict )
 
     def isPublic( self, name: str ) -> bool:
         # Entity is considered public if public is not needed or entities are public by default and entity is not specifically marked as private or entity is specifically marked as public
@@ -41,6 +42,7 @@ class PublicObj:
 # Set the namespace as Fxtran for XPath expressions
 ns = {'fx': 'http://fxtran.net/#syntax'}
 
+
 # TODO hc
 def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/input/fxtran/standaloneXML/subfolder/cfo_sut_example.xml', need_public: bool = False, modules: List[ ModuleSymbol ] = [ ] ) -> Tuple[
     List[ Tuple[ str ] ], List[ Tuple[ str, str, List[ str ] ] ] ]:
@@ -51,7 +53,7 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
     # extract names of module symbols for scope filter
     moduleNames = list( map( lambda s: s.name, modules ) )
     # extract filename of original file
-    basename = os.path.splitext(os.path.basename(xmlPath))[0]
+    basename = os.path.splitext( os.path.basename( xmlPath ) )[ 0 ]
     baseModule: ModuleSymbol = None
 
     # Get the root element
@@ -64,7 +66,7 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
     variables = [ ]  # Variables to return (EN-decl elements within T-decl-stmt elements)
     scopes = [ ]  # Scopes to return
     scopeStack = [ ]  # Stack to track the current scope
-    pubElement: PublicObj = PublicObj()  # Elements declared as public
+    pubElement: PublicObj = PublicObj( needPublic = need_public )  # Elements declared as public
 
     # TODO deprecated
     # Scope-changing elements
@@ -105,10 +107,11 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
         if element.tag.endswith( tuple( dyn_end_scope_elements ) ):
 
             # Update return name of first functions without result statement
-            if element.tag.endswith( 'function-stmt' ):
-                for scope in reversed(scopes):
-                    if scope[3] == -1:
-                        scope[3] = lastValueDefined
+            scopeName = nameElement.find( './/fx:n', ns ).text if nameElement is not None else None
+            if element.tag.endswith( 'function-stmt' ) and pubElement.isPublic( scopeName ):
+                for scope in reversed( scopes ):
+                    if scope[ 3 ] == -1:
+                        scope[ 3 ] = lastValueDefined
                         break
 
             scopeStack.pop( )
@@ -119,7 +122,7 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
 
         # Extend the scope stack when entering scopes
         elif element.tag.endswith( tuple( dyn_scope_elements ) ):
-            # Extract scope namedosfsck logical sector size zero
+            # Extract scope name logical sector size zero
             scopeName = nameElement.find( './/fx:n', ns ).text
 
             if stmtName == 'module':
@@ -133,7 +136,7 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
                 # Check if top level scope is in filtered scope or if filtered scope is empty
                 if not scopeStack and scopeName in moduleNames:
                     isFilteredScope = True
-                    module: ModuleSymbol = next(filter ( lambda module: module.name == scopeName, modules))
+                    module: ModuleSymbol = next( filter( lambda module: module.name == scopeName, modules ) )
 
                     # get filename of original file
                     module.file = basename
@@ -160,7 +163,9 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
                         resultID = -1
 
                 # Add type, name and arguments to returning scopes
-                scopes.append( [stmtName, scopeName, argumentNames,resultID, currentScope] )
+                # Check public availability
+                if pubElement.isPublic( scopeName ):
+                    scopes.append( [ stmtName, scopeName, argumentNames, resultID, currentScope ] )
 
             # Update scope stack
             scopeStack.append( scopeName )
@@ -174,13 +179,23 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
         elif element.tag.endswith( 'public-stmt' ):
             pubIds = list( map( (lambda itm: itm.text), element.findall( './/fx:n', ns ) ) )
             for item in pubIds:
-                pubElement.pubElements[item] = pubElement.pubElements.get(item, item)
+                pubElement.pubElements[ item ] = pubElement.pubElements.get( item, [ ] )
 
+        # Store private available ids
+        elif element.tag.endswith( 'private-stmt' ):
+            prIds = list( map( (lambda itm: itm.text), element.findall( './/fx:n', ns ) ) )
+
+            # Elements are private by default if no variable is given. Else extract private elements.
+            if not prIds:
+                pubElement.defaultPrivate = True
+            else:
+                for item in prIds:
+                    pubElement.prElements[ item ] = pubElement.prElements.get( item, [ ] )
 
         # Extract variables, public only, if needed
         elif element.tag.endswith( 'contains-stmt' ):
             # Check if top level scope is in filtered scope or if filtered scope is empty
-            if len(scopeStack) == 1 and scopeStack[0] == baseModule.name:
+            if len( scopeStack ) == 1 and scopeStack[ 0 ] == baseModule.name:
                 baseModule.containsFunction = True
 
         # Extract variables, public only, if needed
@@ -190,27 +205,35 @@ def filterXML( xmlPath: str = '/home/sgu/Documents/python-oceandsls/tdd-dsl/inpu
             if isFilteredScope:
                 attributes = list( map( lambda itm: itm.text, element.findall( './/fx:attribute-N', ns ) ) )
 
+                # TODO deprecated?
                 # Check if variable is not an output
-                intentSpec = element.find( './/fx:intent-spec', ns )
-                if intentSpec is None or intentSpec.text != 'out':
-                    # Get the current scope from the scope stack
-                    currentScope = '.'.join( scopeStack )
+                # intentSpec = element.find( './/fx:intent-spec', ns )
+                # if intentSpec is None or intentSpec.text != 'out':
 
-                    # Get the type of the variable if it exists
-                    tSpecElement = element.find( './/fx:T-N', ns )
-                    variableType = tSpecElement.text if tSpecElement is not None else ''
+                # Get the current scope from the scope stack
+                currentScope = '.'.join( scopeStack )
 
-                    for enDecl in element.findall( './/fx:EN-decl', ns ):
-                        # Get the name of the variable from the named element
-                        variableName = enDecl.find( './/fx:n', ns ).text
-                        # Save name for return type of functions
-                        lastValueDefined = variableName
+                # Get the type of the variable if it exists
+                tSpecElement = element.find( './/fx:T-N', ns )
+                variableType = tSpecElement.text if tSpecElement is not None else ''
 
-                        # TODO check hc PUBLIC
-                        # Check public availability
-                        if variableName in pubElements or 'PUBLIC' in attributes or not need_public:
-                            # Save the variable names with their respective types and scopes
-                            variables.append( (variableName, variableType, currentScope) )
+                for enDecl in element.findall( './/fx:EN-decl', ns ):
+                    # Get the name of the variable from the named element
+                    variableName = enDecl.find( './/fx:n', ns ).text
+
+                    # Add element to public object if Public attribute is found or extend attributes if element is already public
+                    pubElementEntry = pubElement.pubElements.get( item, attributes )
+                    # TODO hc Public
+                    if pubElementEntry or 'PUBLIC' in attributes:
+                        pubElement.pubElements[ variableName ] = pubElementEntry
+
+                    # Save name for return type of functions
+                    lastValueDefined = variableName
+
+                    # Check public availability
+                    if pubElement.isPublic( variableName ):
+                        # Save the variable names with their respective types and scopes
+                        variables.append( (variableName, variableType, currentScope) )
 
     # TODO Debug
     # Print the list of found named scopes

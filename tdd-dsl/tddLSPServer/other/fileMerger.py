367bd1ad0b9f57d7b22cf47dd774788568ef452c
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-08-08 17:29:50 +0200

    TDD-DSL: Update filestructure. Add merge function method

--- tdd-dsl/tddLSPServer/other/fileMerger.py
+++ tdd-dsl/tddLSPServer/other/fileMerger.py
@@ -5,7 +5,7 @@ __author__ = 'sgu'
 import re
 import os
 
-def merge_fortran_function(fortran_file, function_name, function_code, module_name: str = ''):
+def merge_fortran_operation( fortran_file, function_name, function_code, module_name: str = '' ):
     # Read the contents of the Fortran file
     with open( fortran_file, mode = 'r', encoding = 'utf-8' ) as f:
         content = f.read( )
@@ -16,23 +16,27 @@ def merge_fortran_function(fortran_file, function_name, function_code, module_na
 
     contains_pattern = r'\n *contains *\n'
     function_end_pattern = r'(\n *end +(?:subroutine|function) +' + function_name + ' *\n)'
-
     module_end_pattern = r'(\n *end +module +' + module_name + ' *\n?)'
+
 
     # Find the position to insert the new code
     match_contains = re.search(contains_pattern, content, flags=re.IGNORECASE)
     match_function = re.search(function_end_pattern, content, flags=re.IGNORECASE)
+    match_module = re.search(module_end_pattern, content, flags=re.IGNORECASE)
+
 
     if match_function:
-        # Insert the new code after the "contains" statement
+        # Insert the new code after the function/subroutine
         insert_position = match_function.end()
     elif match_contains:
-        # Insert the new code after the function/subroutine
+        # Insert the new code after the "contains" statement
         insert_position = match_contains.end()
+    elif match_module:
+        # Insert the new code at the module end
+        insert_position = match_module.start()
     else:
         # If neither "contains" nor the function/subroutine is found, raise an error
-        raise ValueError(f'Function or "contains" statement not found: {function_name}')
-
+        raise ValueError(f'Function, Module or "contains" statement not found. Function: {function_name}, Module: {module_name}')
     # Insert the new code at the determined position
     modified_content = content[:insert_position] + function_code + '\n' + content[insert_position:]
 
@@ -44,10 +48,10 @@ def merge_fortran_function(fortran_file, function_name, function_code, module_na
 # debug usage
 fortran_file = 'cfo_sut_example.f90'
 function_name = 'fT_ME'
-module_name = 'fT_ME'
+module_name = 'cfo_example'
 function_code = '''
   subroutine my_function(arg1, arg2)
     ! Fortran function code goes here
   end subroutine my_function'''
 
-merge_fortran_function(fortran_file, function_name, function_code)
+merge_fortran_operation( fortran_file, function_name, function_code, module_name )

a93dd67e6d282ed6af710cca2a765fb4bb6554fc
Sven Gundlach <sven.gundlach@email.uni-kiel.de>
2023-11-18 16:25:24 +0100

    fix example

--- tdd-dsl/input/tdd_dev/UVic_example_opem/additional_use_3.f90
+++ tdd-dsl/input/tdd_dev/UVic_example_opem/additional_use_3.f90
@@ -3,43 +3,8 @@ MODULE additional_use_3
 
   PRIVATE
   PUBLIC :: fT_ME, foo
-  PUBLIC :: fT_ME, foo
-
-
 
 CONTAINS
-
-  ! auto-generated, src: tdd-dsl/input/test_fT_ME.tdd, 12:4-19:8
-  FUNCTION fT_ME (arg0, unit) RESULT (out)
-    IMPLICIT NONE
-    real(dp), INTENT(IN)  :: arg0
-    real :: out
-
-    character(len = *),optional :: unit
-    if(present(unit))then
-        if ( unit /= "K" ) then
-          print *, "fT_ME: Expected K as input unit but got ", unit
-        endif
-    endif
-
-    out = ! <<Add return expression here>>
-  END FUNCTION fT_ME
-
-  ! auto-generated, src: tdd-dsl/input/test_fT_ME.tdd, 36:4-42:52
-  FUNCTION foo (arg0, unit) RESULT (out)
-    IMPLICIT NONE
-    real(dp), INTENT(IN)  :: arg0
-    real :: out
-
-    character(len = *),optional :: unit
-    if(present(unit))then
-        if ( unit /= "K" ) then
-          print *, "foo: Expected K as input unit but got ", unit
-        endif
-    endif
-
-    out = ! <<Add return expression here>>
-  END FUNCTION foo
 
   ! auto-generated, src: tdd-dsl/input/test_fT_ME.tdd, 12:4-19:8
   FUNCTION fT_ME (arg0, unit) RESULT (out)
